c50341ba4a8c13a106972f5c913d7341aa2ffcac
Jiro.Kirameki <hida@triton.ocn.ne.jp>
2017-11-12 16:49:15 +0000

    for heroku

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,13 +2,18 @@ class MessagesController < ApplicationController
   def index
     @messages = Message.all
   end
-
+  
+  def show
+    @message = Message.find(params[:id])
+  end
+  
   def new
     @message = Message.new
   end
-
+  
   def create
     @message = Message.new(message_params)
+
     if @message.save
       flash[:success] = 'Message が正常に投稿されました'
       redirect_to @message
@@ -17,13 +22,14 @@ class MessagesController < ApplicationController
       render :new
     end
   end
-  
+
   def edit
     @message = Message.find(params[:id])
   end
-  
+
   def update
     @message = Message.find(params[:id])
+
     if @message.update(message_params)
       flash[:success] = 'Message は正常に更新されました'
       redirect_to @message
@@ -40,13 +46,11 @@ class MessagesController < ApplicationController
     flash[:success] = 'Message は正常に削除されました'
     redirect_to messages_url
   end
-  
+
   private
-  
+
+  # Strong Parameter
   def message_params
     params.require(:message).permit(:content)
   end
 end
-
-
-
